4bc4d270a23b1e3274dfe14b3a97c0ca7bd1485e
Cesar Oliva <you@example.com>
2021-06-28 12:50:36 -0500

    guardado efectos

--- Javascript/efectos.js
+++ Javascript/efectos.js
@@ -5,7 +5,7 @@ const typed = new Typed('.typed', {
     ],
     stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
     typeSpeed: 100, // Velocidad en mlisegundos para poner una letra,
-    startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+    startDelay: 500, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
     backSpeed: 100, // Velocidad en milisegundos para borrrar una letra,
     smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
     shuffle: false, // Alterar el orden en el que escribe las palabras.
