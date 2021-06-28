8ae9ef5648ce18bc64cd20f0ee30708dfbac1075
Cesar Oliva <you@example.com>
2021-06-28 12:16:54 -0500

    nav

--- Javascript/scroll.js
+++ Javascript/scroll.js
@@ -42,4 +42,45 @@ $(document).ready(function(){
         }, 1000);
     })
 
+    //medias
+    $(window).scroll(function(){
+        var windowWidth = $(window).width();
+
+        if (windowWidth > 485){
+            //dom
+            var nav = document.createElement('div')
+            padre.setAttribute('class', "contenedor-navbar")
+            padre.innerHTML =  `
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuNavegacion" aria-controls="menuNavegacion" aria-expanded="false" aria-label="Alternar menu">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="menuNavegacion">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" id="btn-sobre-mi" href="#">Sobre-mi</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" id="btn-conocimientos">Conocimientos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="btn-trabajos" href="#">Trabajos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" id="btn-contacto">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            </nav>
+            `
+
+            var contenido = document.getElementsByTagName('header')
+            contenido.appendChild(nav)
+        }
+    });
+
 })
